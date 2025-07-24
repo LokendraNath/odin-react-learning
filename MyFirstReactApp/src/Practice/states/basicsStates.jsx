641be8c6c34964754145f1eb42980d43d837f091
Lokendra Nath <nathdeveloper25@gmail.com>
2025-07-24 17:08:28 +0530

    state structure

--- MyFirstReactApp/src/Practice/states/basicsStates.jsx
+++ MyFirstReactApp/src/Practice/states/basicsStates.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 // import "./App.css";
-
-const COLORS = ["pink", "green", "blue", "yellow", "purple"];
-
+/*
 export function BasicColorClick() {
   const [name, setName] = useState({ first: "", last: "" });
 
@@ -43,5 +41,106 @@ export function BasicColorClick() {
         {name.first} {name.last}
       </p>
     </div>
+  );
+}
+*/
+
+/*
+export default function FeedbackForm() {
+  const [text, setText] = useState("");
+  const [status, setStatus] = useState("typing");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus("sending");
+    await sendMessage(text);
+    setStatus("sent");
+  }
+
+  const isSending = status === "sending";
+  const isSent = status === "sent";
+
+  if (isSent) {
+    return <h1>Thanks for feedback!</h1>;
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+            <h1 className="text-4xl mb-5"></h1>
+      <p>How was your stay at The Prancing Pony?</p>
+      <textarea
+        disabled={isSending}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <br />
+      <button disabled={isSending} type="submit">
+        Send
+      </button>
+      {isSending && <p>Sending...</p>}
+    </form>
+  );
+}
+
+// Pretend to send a message.
+function sendMessage(text) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
+}
+*/
+
+
+const initialItems = [
+  { title: "pretzels", id: 0 },
+  { title: "crispy seaweed", id: 1 },
+  { title: "granola bar", id: 2 },
+];
+
+export default function Menu() {
+  const [items, setItems] = useState(initialItems);
+  const [selectedId, setSelectedId] = useState(0);
+
+  const selectedItem = items.find((item) => item.id === selectedId);
+
+  function handleItemChange(id, e) {
+    setItems(
+      items.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            title: e.target.value,
+          };
+        } else {
+          return item;
+        }
+      })
+    );
+  }
+
+  return (
+    <>
+      <h2>What's your travel snack?</h2>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <input
+              value={item.title}
+              onChange={(e) => {
+                handleItemChange(item.id, e);
+              }}
+            />{" "}
+            <button
+              onClick={() => {
+                setSelectedId(item.id);
+              }}
+            >
+              Choose
+            </button>
+          </li>
+        ))}
+      </ul>
+      <p>You picked {selectedItem.title}.</p>
+    </>
   );
 }
