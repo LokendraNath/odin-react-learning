2a975af143e9d61ce76a7e700edbd7fc5d482c5c
Lokendra Nath <nathdeveloper25@gmail.com>
2025-07-27 17:25:45 +0530

    remove unuseable text comment

--- MyFirstReactApp/src/App.jsx
+++ MyFirstReactApp/src/App.jsx
@@ -1,28 +1,12 @@
 import "./App.css";
-import { Person } from "./Practice/states/state challenges/OdinChallenge";
-// import Accordion from "./Practice/states/sharing state component/Accordian.jsx";
-// import TravelPlan from "./Practice/states/basic nested/TravelPlan.jsx";
-// import MailClient from "./Practice/states/state challenges/Highlited Letter/MainClient.jsx";
-// } from "./Practice/states/basicsStates.jsx";
-// import FeedbackForm from "./Practice/states/state challenges/Gallary.jsx";
-// import { ProfileC } from "./Practice/Props/ChallengePassingJSXchildren.jsx";
-// import { BasicColorClick } from "./Practice/states/basicsStates.jsx";
-// import Gallery from "./Practice/states/state challenges/Gallary.jsx";
-// import Profile from "./Practice/Props/PassingJSXchildren.jsx";
-// import Gallery from "./Practice/render/Chalenge/PassingJSXchildren.jsx";
-// import Poem from "./Practice/render/Chalenge/ListWithSeperation";
-// import RecipeList from "./Practice/render/Chalenge/recepi3.jsx";
-// import { Food } from "./Practice/Food";
-// import Greeting from "./Practice/Greeting";
-// import { AllPhysicists } from "./Practice/render/Filter/renderFilter.jsx";
-// import { MyFavSeries } from "./Practice/render/rendering";
-// import { TernaryMyFavSeries } from "./Practice/render/Ternary + &&/render";
+import Clock from "./Practice/useEffect/basicEffect";
+
 
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-blue-900 flex flex-col items-center">
-        <Person />
+      <div className="min-h-screen flex flex-col items-center">
+       <Clock />
       </div>
     </>
   );
